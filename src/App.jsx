ca04a6c3335492f52551d817af51a76428452e4e
AngelicaManliguez <angelicamanliguez12@gmail.com>
2025-01-20 19:56:46 +0800

    home

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <>
-      < SignInComponent />
+      {/*< SignInComponent />*/}
+      <HomeComponents/>
     </>
   )
 }
--- src/components/HomeComponents.jsx
+++ src/components/HomeComponents.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import '../style/home.css';
 import { NavbarComponent } from './NavbarComponent';
 
 export const HomeComponents = () => {
   return (
     <>
-        < NavbarComponent />
-        <div>HomeComponents</div>
+        {/*< NavbarComponent />*/}
+        <div className="landing-page">
+            <div className="header-section">
+                < NavbarComponent />
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            
+            <div className="features-section">
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+            
+            <div className='about-section'>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            </div>
+        </div>
+        
     </>
   )
 }
