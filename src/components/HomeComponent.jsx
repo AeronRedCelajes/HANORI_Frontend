6f664475159cc45b33b60da122887564ef20324b
AngelicaManliguez <angelicamanliguez12@gmail.com>
2025-01-21 14:25:18 +0800

    about us

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../style/home.css';
 import { NavbarComponent } from './NavbarComponent';
 import { AboutComponent } from './AboutComponent';
+import { FooterComponent } from './FooterComponent';
 
 export const HomeComponents = () => {
   return (
@@ -25,13 +26,13 @@ export const HomeComponents = () => {
             <div className='about-orange-border'>
                 <div className='about-section'>
                     <AboutComponent />
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 </div>
             </div>
+
+            <div className='footer-section'>
+                <FooterComponent/>
+            </div>
         </div>
-        
     </>
   )
 }
